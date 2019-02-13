e1b411e0268efb62651b79fabb64824e94144ab7
WangTangYu <wtyhainan@163.com>
2019-02-13 17:51:08 +0800

    last

--- tfrecord.py
+++ tfrecord.py
@@ -1,10 +1,15 @@
+'''
+该代码运行在tensorflow Eager Execution 模式下
+'''
 import tensorflow as tf
 import numpy as np
-import os
+import matplotlib.pyplot as plt
 
-def tfencord(data , path):
+def tfencode(data , path):
+
     data = np.array(data , dtype=np.float32)
     writer = tf.python_io.TFRecordWriter(path)
+
     for i in data:
         x = i
         y = 0
@@ -25,71 +30,50 @@ def tfencord(data , path):
         writer.write(example.SerializeToString())
 
 
+def _parse_example(example_proto):
+
+    #定义一个特征字典，该字典的键名必须与tfrecord文件的键名一致，字典的值类型必须与tfrecord文件一致
+    features = {'x':tf.FixedLenFeature([] , tf.string) ,
+                'y':tf.FixedLenFeature([] , tf.int64)}
+
+    #利用tf.parse_single_example读取tfrecord文件，返回一个与特征字典一致的tensor字典
+    parse_example = tf.parse_single_example(example_proto , features = features)
+
+    #对x进行解码。在生成tfrecord文件时，通常我们会将x编码为二进制文件，因此在这里需要采用tf.decode_raw函数解码。在这里需要
+    #注意的是，解码的数据格式必须与编码的数据格式相同，否则会出现错误
+    x = tf.decode_raw(parse_example['x'] , tf.float32)
+    x = tf.reshape(x , shape = [125])
+    y = parse_example['y']
+    return x , y
 
 
-# def tfencord(data , path):
-#
-#     data = np.array(data , dtype = np.float32)
-#     writer = tf.python_io.TFRecordWriter(path)
-#     for i in range(len(data)):
-#         label_value = 0
-#         dat_value = data[i]
-#         raw_dat = tf.train.Feature(bytes_list=tf.train.BytesList(value=[dat_value.tobytes()]))
-#         raw_label = tf.train.Feature(int64_list=tf.train.Int64List(value=[label_value]))
-#
-#         feature = {'y': raw_label, 'x': raw_dat}
-#         example = tf.train.Example(features=tf.train.Features(feature=feature))
-#
-#
-#         writer.write(example.SerializeToString())
-#
+def tfdecode(path):
+    dataset = tf.data.TFRecordDataset([path])
+    dataset = dataset.map(_parse_example)
+    dataset = dataset.repeat(1)
+    # dataset = dataset.shuffle(10000)
+    dataset = dataset.batch(1)
 
-# def _parse_example(example_proto):
-#
-#     features = {'x': tf.FixedLenFeature([] , tf.string) ,
-#                 'y' : tf.FixedLenFeature([] , tf.int64)}
-#
-#     parse_features = tf.parse_single_example(example_proto , features = features)
-#
-#     x = tf.decode_raw(parse_features['x'] , tf.float32)
-#
-#     x = tf.reshape(x , shape = [125])
-#
-#     y = parse_features['y']
-#
-#     return x , y
-#
-#
-# def train_input_fn(params):
-#
-#     train_tfrecord_path = ['/home/wty/桌面/test.tfrecords']
-#
-#     dataset = tf.data.TFRecordDataset(train_tfrecord_path)
-#     dataset = dataset.map(_parse_example)
-#     dataset = dataset.repeat(1)
-#     # dataset = dataset.shuffle(10000)
-#     dataset = dataset.batch(1)
-#
-#
-#     iterator = dataset.make_one_shot_iterator()
-#
-#
-#     labels = []
-#     num = 0
-#     from collections import Counter
-#     try:
-#         while(1):
-#             x, y = iterator.get_next()
-#             print(x)
-#             num += 1
-#     except:
-#         labels = np.reshape(labels , [-1])
-#
-#         print(Counter(labels))
-#         print('num is : ' , num)
-#
-#
-#
+    iterator = dataset.make_one_shot_iterator()
+    num = 0
+    dat = []
+    try:
+        while(1):
+            x , y = iterator.get_next()
+            dat.append(np.array(x[0]))
+            num += 1
+    except:
+        print(num)
+
+        fig , ax = plt.subplots(3 , 4)
+        for i in range(3):
+            for j in range(4):
+                ax[i][j].plot(dat[i * 3 + j])
+
+        plt.show()
+
+
+
 
 
 
